cc7adaa0869b1af5b575755f0e03ff6486734783
srobertson90 <stuart.robertson.applications@gmail.com>
2016-09-23 10:59:35 +0100

    adding files and dependancies

---
+++ specs/record_spec.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+var Record = require('../record');
+
+describe('Record', function(){
+
+  var test;
+  var empty;
+
+  before(function(){
+    test = new Record({id: "00523", artist: "Amon Amarth",title: "Sutur Rising", price: 8.99});
+    empty = new Record();
+  });
+
+  it('should have an account number', function(){
+    assert.equal("00523", test.id);
+  });
+
+  it('should have an associated artist', function(){
+    assert.equal("Amon Amarth", test.artist);
+  });
+
+  it('should have an album title', function(){
+    assert.equal("Sutur Rising", test.title);
+  });
+
+  it('should have a price', function(){
+    assert.equal(8.99, test.price);
+  });
+});
+
