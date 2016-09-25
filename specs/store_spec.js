4bd9398559cbec500bd7d9e8159dcb8b19123ecc
srobertson90 <stuart.robertson.applications@gmail.com>
2016-09-25 20:36:39 +0100

    added test framework for store, made store company test

---
+++ specs/store_spec.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var Store = require('../store');
+
+describe('Store', function(){
+
+  var testStore;
+  var genericStore;
+
+  before(function(){
+    testStore = new Store({company: "Harveys", city: "Glasgow", balance: 2147});
+    genericStore = new Store();
+  });
+
+  it('should have an associated company', function(){
+    assert.equal("Harveys", testStore.company);
+  });
+
+
+});
