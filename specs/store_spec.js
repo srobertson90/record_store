00783fe1622f511e22e5f6520acdc990319c1c09
srobertson90 <stuart.robertson.applications@gmail.com>
2016-09-25 20:55:09 +0100

    added should start with balance test, working as intended

--- specs/store_spec.js
+++ specs/store_spec.js
@@ -23,11 +23,16 @@ describe('Store', function(){
     assert.deepEqual([], testStore.records);
   });
 
+  it('should have a balance', function(){
+    assert.equal(2147, testStore.balance);
+  });
+
   it('should be able to add a record', function(){
     testStore.addRecord({artist: "Amon Amarth",title: "Sutur Rising", price: 8.99});
     assert.deepEqual([{id: "00001", artist: "Amon Amarth",title: "Sutur Rising", price: 8.99}], testStore.records);
   });
 
-  
+
+
 
 });
